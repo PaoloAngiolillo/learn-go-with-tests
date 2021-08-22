86e91b236387c349191e29f230cc2fac596a904c
Paolo Angiolillo <paoloangiolillo@Paolos-MacBook-Pro.local>
2021-08-22 12:28:49 +0200

    Added Maps and dependency injection

---
+++ dependency-injection/di.go
@@ -0,0 +1,23 @@
+package dependency_injection
+
+import (
+	"fmt"
+	"io"
+)
+
+import (
+	"log"
+	"net/http"
+)
+
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	log.Fatal(http.ListenAndServe(":5000", http.HandlerFunc(MyGreeterHandler)))
+}
